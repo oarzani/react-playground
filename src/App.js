1f641b8dcbfe051992c83e3dc08434f07714ca9a
Omid Arzani <omid.arzani@gmx.net>
2019-09-20 09:29:14 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
-
+import { Header } from "./header";
 import "./App.css";
 import PasswordInput from "./components/PasswordInput";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <PasswordInput />
       <PasswordInput />
     </div>
   );
